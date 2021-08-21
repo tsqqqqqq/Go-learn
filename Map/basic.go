e97ce28c29719dfb73c516ebafba7c509e4b032f
tsqqqqqq <563068223@qq.com>
2021-08-21 12:25:04 +0800

    新增map学习代码

---
+++ Map/basic.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/**
+map 的key使用hash表 ，
+slice map function 不可以作为key
+Struct类型不包含上述字段也可以作为key
+*/
+func main() {
+	//create map
+	m1 := map[string]string{"name": "tsq", "age": "18"}
+
+	m2 := make(map[string]string) //empty 开辟了内存空间
+
+	var m3 map[string]string //nil 为nil 可以做运算
+
+	fmt.Println(m1, m2, m3)
+
+	//loop 无序
+	for k, v := range m1 {
+		fmt.Println(k, v)
+	}
+	//需要手动的先把key放到slice里排序，根据排序规则，
+	//再遍历slice中的key，获取map
+
+	//get Value
+	name := m1["name"]
+	fmt.Println(name)
+
+	//check key
+	sex, ok := m1["sex"]
+	if ok {
+		fmt.Println(sex)
+	} else {
+		fmt.Println("不存在当前key")
+	}
+
+	//delete values
+	delete(m1, "sex")
+
+}
---
+++ Map/demo1.go
@@ -0,0 +1,9 @@
+package main
+
+/**
+寻找最长不含有重复字符的子串
+abcabcbb -> abc
+*/
+func main() {
+
+}
